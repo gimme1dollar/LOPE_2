a8d6dd91ec69a6104a16032d129240a84ca11f30
조승혁 <seunghyuk.cho@navercorp.com>
2020-09-13 15:04:37 +0900

    Fixed data model

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Meme, Detail
+from .models import Meme, Detail, Property
 
 admin.site.register(Meme)
+admin.site.register(Property)
 admin.site.register(Detail)
